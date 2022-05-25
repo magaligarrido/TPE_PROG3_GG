2c5c72b5ad1f3c64bdfe6bde561236f5466bdc74
Magali Garrido <magalisuyaygarrido@gmail.com>
2022-05-25 20:43:24 -0300

    Inicio del trabajo Especial

--- src/Genero.java
+++ src/Genero.java
@@ -1,4 +1,46 @@
+import java.util.ArrayList;
 
 public class Genero {
-
+	private String genero;
+	private ArrayList<Libro> libros;
+	public Genero(String genero) {
+		super();
+		this.genero = genero;
+		this.libros = new ArrayList<>();
+	}
+	public String getGenero() {
+		return genero;
+	}
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+	
+	public void addLibro(Libro l) {
+		if(!libros.contains(l)) {
+			this.libros.add(l);
+		}
+	}
+	
+	public void deleteLibro(Libro l) {
+		if(libros.contains(l)) {
+			for(int i=0; i<libros.size(); i++) {
+				if(libros.get(i).equals(l)) {
+					this.libros.remove(i);
+					break;
+				}
+			}
+		}
+	}
+	
+//	public boolean equals(Object o) {
+//		Genero g = (Genero) o;
+//		if(this.getGenero().equals(g.getGenero())){
+//			for(int i=0; i < this.libros.size(); i++) {
+//				if (g.)
+//			}
+//		}
+//	}
+//	IMPLEMENTAR COPY
+	
+	
 }
--- src/Libro.java
+++ src/Libro.java
@@ -1,4 +1,60 @@
+import java.util.ArrayList;
 
 public class Libro {
+	private String titulo, autor;
+	private int cant_paginas;
+	private ArrayList<String> generos;
+	
+	public Libro(String titulo, String autor, int cant_paginas) {
+		super();
+		this.titulo = titulo;
+		this.autor = autor;
+		this.cant_paginas = cant_paginas;
+		this.generos = new ArrayList<>();
+	}
 
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public int getCant_paginas() {
+		return cant_paginas;
+	}
+
+	public void setCant_paginas(int cant_paginas) {
+		this.cant_paginas = cant_paginas;
+	}
+	
+	public void addGenero(String g) {
+		if(!generos.contains(g)) {
+			this.generos.add(g);
+		}
+	}
+	
+	public void deleteGenero(String g) {
+		if(generos.contains(g)) {
+			for(int i=0; i < this.generos.size(); i++) {
+				if(generos.get(i).equals(g)) {
+					this.generos.remove(i);
+					break;
+				}
+			}
+		}
+	}
+	
+	//IMPLEMENTAR EQUALS PARA LIBRO
+	
+	
 }
